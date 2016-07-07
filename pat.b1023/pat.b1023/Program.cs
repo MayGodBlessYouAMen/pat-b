803d8414e774ff59bbe3e98ae31ccaa264995803
gargoyle <gargoyle_shadow@qq.com>
2016-07-07 22:24:17 +0900

    pat.b1023

---
+++ pat.b1023/pat.b1023/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace pat.b1023
+{
+    /// <summary>
+    /// PAT.1023
+    /// 2016.05.10
+    /// by XY
+    /// at warabi
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            string input = Console.ReadLine();
+            string[] inputlist = input.Split(' ');
+            int[] count = new int[10];
+            for (int i = 0; i < inputlist.Length; i++)
+            {
+                count[i] = int.Parse(inputlist[i]);
+            }
+
+            string output = "";
+            if (count[0] > 0)
+            {
+                int first = 0;
+                for (int i = 1; i < 10; i++)
+                {
+                    if (count[i] > 0)
+                    {
+                        output += i;
+                        count[i]--;
+                        first = i;
+                        break;
+                    }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < count[i]; j++)
+                    {
+                        output += i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < count[i]; j++)
+                    {
+                        output += i;
+                    }
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
