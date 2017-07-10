7ced91a26d078bf49266a048edde266c1429d3b9
gargoyle <gargoyle_shadow@qq.com>
2017-07-10 17:15:01 +0800

    create pat.b1070

---
+++ pat.b1070/pat.b1070/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pat.b1070
+{
+    /// <summary>
+    /// pat.b1070. 结绳(25)
+    /// 2017.07.10
+    /// by XY
+    /// at NingBo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            #region 思路
+            /*
+             *先排序，然后
+             *
+             *s1=a1
+             *s2=(s1+a2)/2
+             *...
+             *sn=(s(n-1)+an)/2
+             *
+             *可以用递归
+             */
+            #endregion
+
+        }
+
+    }
+}
