6dafec6bd3c3961434ab7eb301869844627bb72a
gargoyle <gargoyle_shadow@qq.com>
2017-07-10 14:07:21 +0800

    create pat.b1055

---
+++ pat.b1055/pat.b1055/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pat.b1055
+{
+    /// <summary>
+    /// 1055. 集体照 (25)
+    /// 2017.07.10
+    /// by XY
+    /// at NingBo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            //TODO
+
+        }
+    }
+}
