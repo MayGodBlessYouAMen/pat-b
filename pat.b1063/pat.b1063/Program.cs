5ab8d78682da802d4138dda98e8ed729dadad3d0
gargoyle <gargoyle_shadow@qq.com>
2017-09-13 11:53:56 +0800

    2017.09.13 一次AC
    2017.09.13 一次AC

---
+++ pat.b1063/pat.b1063/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pat.b1063
+{
+    /// <summary>
+    /// pat.b1063. 计算谱半径(20)
+    /// 2017.09.13
+    /// by XY
+    /// at NingBo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double r = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] args = Console.ReadLine().Split(' ');
+                int a = int.Parse(args[0]);
+                int b = int.Parse(args[1]);
+                double temp = Math.Round(Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)), 2);
+                if (r < temp)
+                {
+                    r = temp;
+                }
+            }
+            Console.WriteLine(r.ToString("0.00"));
+        }
+
+    }
+}
